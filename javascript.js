b0832bfff876424a0b68d692066b37ff98898b4e
Trizah Njeri <trizahn2002@gmail.com>
2026-02-13 12:33:29 +0300

    Adding jobs in js

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// Get existing from localStorage
+let jobs =JSON.parse(localStorage.getItem("jobs"));
+
+//Save jobs to localStorage
+function saveJobs() {
+  localStorage.setItem("jobs", JSON.stringify(jobs));
+}
+//Add Job
+const form = document.getElementById("jobForm");
+if (form {
+  form.addEventListener("submit", function (e) {e.preventDefault();
+    const company
+  })
+}) 
