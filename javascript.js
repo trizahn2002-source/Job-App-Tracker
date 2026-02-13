43cc358f7cab66e6ff523637e225ce5cbbab6ad7
Trizah Njeri <trizahn2002@gmail.com>
2026-02-13 13:50:10 +0300

    Display jobs on js

--- javascript.js
+++ javascript.js
@@ -9,6 +9,29 @@ function saveJobs() {
 const form = document.getElementById("jobForm");
 if (form {
   form.addEventListener("submit", function (e) {e.preventDefault();
-    const company
-  })
+    const company = document.getElementById("company").value;
+    const position = document.getElementById("position").value;
+    const status = document.getElementById("status").value;
+    //Crete job object
+    const newJob = {
+      company: company,
+      position: position,
+      status: status
+    };
+    //Add to array
+    jobs.push(newJob);
+
+    //Save
+    saveJobs();
+
+    //Reset form
+    form.reset();
+
+    //Optional message
+    alert("Job added successfully!");
+  });
+
+  //Display the jobs
+  const container =
+
 }) 
